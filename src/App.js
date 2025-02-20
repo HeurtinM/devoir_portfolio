78abeade306cb72038be2544d91b4bb59f2d6844
Heurtin Manu <manu.heurtin@gmail.com>
2025-02-20 13:29:29 +0100

    ajout de router et des 5 pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Services from "./pages/services";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+import LegalNotice from "./pages/legal_notice";
 
 class App extends React.Component{
   render(){
     return(
       <div className="container">
         <header>
-          <h1>Hello world</h1>
+          <nav>
+            <a href="/">Acceuil</a>
+            <a href="services">Services</a>
+            <a href="projects">Réalisations</a>
+            <a href="contact">Contact</a>
+            <a href="legal_notice">Mentions légals</a>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="services"  element={<Services/>}></Route>
+            <Route path="projects"  element={<Projects/>}></Route>
+            <Route path="contact"  element={<Contact/>}></Route>
+            <Route path="legal_notice"  element={<LegalNotice/>}></Route>
+          </Routes>
         </header>
         <main>
 
---
+++ src/pages/legal_notice.jsx
@@ -0,0 +1,9 @@
+const LegalNotice = () =>{
+    return(
+        <div>
+            <h1>Mentions légals</h1>
+        </div>
+    )
+}
+
+export default LegalNotice;
