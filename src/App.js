04aa09a6bba0f4c808e0f9ecce04d978a1071b48
Heurtin Manu <manu.heurtin@gmail.com>
2025-02-24 12:03:10 +0100

    ajout de la feature pour changer la couleur du nav en fonction de la page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "./pages/home";
 import Services from "./pages/services";
@@ -7,17 +7,18 @@ import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import LegalNotice from "./pages/legal_notice";
 
-class App extends React.Component{
-  render(){
+const App = () => {
+  const location = useLocation(); //le brief envoyait a https://api.reactrouter.com/v7/functions/react_router.NavLink.html pour la feature de changer le liens nav actif de couleur mais je ne me souvient pas avoir vue ça dans les cours et je n'ai pas réussi a bien comprendre, a la place j'utilise un hook pour determiner le "nav" actif et change son nom de class pour style.css
+    
     return(
       <div className="container">
         <header>
           <nav>
-            <a href="/">Acceuil</a>
-            <a href="services">Services</a>
-            <a href="projects">Réalisations</a>
-            <a href="contact">Contact</a>
-            <a href="legal_notice">Mentions légals</a>
+            <a href="/" className={location.pathname === '/' ? 'active' : ''}>Acceuil</a>
+            <a href="services" className={location.pathname === '/services' ? 'active' : ''} >Services</a>
+            <a href="projects" className={location.pathname === '/projects' ? 'active' : ''}>Réalisations</a>
+            <a href="contact" className={location.pathname === '/contact' ? 'active' : ''}>Contact</a>
+            <a href="legal_notice" className={location.pathname === '/legal_notice' ? 'active' : ''}>Mentions légals</a>
           </nav>
 
           <Routes>
@@ -37,6 +38,6 @@ class App extends React.Component{
       </div>
     )
   }
-}
+
 
 export default App;
