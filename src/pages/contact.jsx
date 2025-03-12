06458cdbcd0394520dcd23987d6dcdc608b012ee
Heurtin Manu <manu.heurtin@gmail.com>
2025-03-12 19:53:22 +0100

    commencé la page adresse

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,24 @@ const Contact = () =>{
     return(
         <div>
             <h1>Contact</h1>
+            <p>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact</p>
+            <section>
+                <form action="">
+                    <h2>Formulaire de contact</h2>
+                    <input type="text" placeholder="Votre nom"/>
+                    <input type="text" placeholder="Votre adresse email"/>
+                    <input type="text" placeholder="Votre numero de téléphone"/>
+                    <input type="text" placeholder="Sujet"/>
+                    <input type="text" placeholder="Votre message"/>
+                    <button type="submit">Envoyer</button>
+                </form>
+                <section>
+                    <h2>John Doe</h2>
+                    <address>
+                        
+                    </address>
+                </section>
+            </section>
         </div>
     )
 }
