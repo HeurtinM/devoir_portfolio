dc02eb1cc660ca2eee2ba588191d9e6cf5045864
Heurtin Manu <manu.heurtin@gmail.com>
2025-03-24 13:26:26 +0100

    page d'acceuil fini (-la modale qui reste a faire)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,4 @@
 import JohnDoePic from "../images/john-doe-about.jpg"
-import HeroBg from "../images/hero-bg.jpg"
 import React, { useEffect, useState } from 'react';
 
 const Home = () =>{
@@ -43,50 +42,80 @@ const Home = () =>{
     }
 
 
-    useEffect(()=>{ //j'ai cru comprendre qu'il était preferable d'utiliser plusieurs hooks useEffect plutot que de mettre differente logique dans un seul
+    useEffect(()=>{ //j'ai cru comprendre qu'il était preferable d'utiliser plusieurs hooks useEffect plutot que de mettre differente logiques dans un seul
         getUser();
     },[])
     return(
         <div>
-            <section>
-                <h1>Bonjour, je suis John Doe</h1> 
-                <h2>Développeur web full stack</h2>
-                <button id="buttonForModal">En savoir plus</button>
-                <img src={HeroBg} alt="developpeur en travail vue de dos" />
+         <div className="bgImage container-fluid d-flex justify-content-center align-items-center">
+            <section className="text-center">
+                <h1 className="display-1 fw-bold text-light">Bonjour, je suis John Doe</h1> 
+                <h2 className="display-3 fw-bold text-light">Développeur web full stack</h2>
+                <button id="buttonForModal" className="btn btn-danger">En savoir plus</button>
             </section>
-            <div id="myModal" class="modal">
+            <div id="myModal" className="modal">
              <div class="modal-content">
               <span class="close">&times;</span>
               <section>
-              {user.login}
-              {user.location}
-              {user.bio}
-              {user.public_repos}
-              {user.followers}
-              {user.following}
+                <img src={user.avatar_url} alt="image de profile de John Doe" /> {/* je ne decrit pas l'avatar dans l'alt car il pourrait changer*/}
+                {user.login}
+                {user.location}
+                {user.bio}
+                {user.public_repos}
+                {user.followers}
+                {user.following}
               </section>
              </div>
             </div>
-            <section>
-                <h3>A propos</h3>
-                <img src={JohnDoePic} alt="John Doe a son bureau" /> {/*j'ecrit l'alt comme si il s'agissait bel et bien d'une image de John Doe et pas juste une stock image*/}
-                <article>
-                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nisi beatae nemo cupiditate veniam laborum sequi? Eveniet omnis quas, cumque, dolor tempore vitae quae non, tempora perspiciatis officiis eius vel.</p>
-                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nisi beatae nemo cupiditate veniam laborum sequi? Eveniet omnis quas, cumque, dolor tempore vitae quae non, tempora perspiciatis officiis eius vel.</p>
-                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nisi beatae nemo cupiditate veniam laborum sequi? Eveniet omnis quas, cumque, dolor tempore vitae quae non, tempora perspiciatis officiis eius vel.</p>
-                </article>
-            </section>
-            <section>
-                <h3>A propos</h3>
-                <article>
-                <label>HTML5 <progress value="90" max="100">90 %</progress></label>
-                <label>CSS3 <progress value="80" max="100">80 %</progress></label>
-                <label>Javascript <progress value="70" max="100">70 %</progress></label> {/*ne pas oublier de passer ça en maj avec le css*/}
-                <label>PHP <progress value="60" max="100">60 %</progress></label>
-                <label>React <progress value="50" max="100">50 %</progress></label> 
-                </article>
-
-            </section>
+         </div>
+            <div className="container border shadow my-3">
+                <div className="row">
+                    <section className="col-sm-6">
+                        <h3 className="fw-bold border-bottom border-primary border-5 pb-2 my-4">A propos</h3>
+                        <img src={JohnDoePic} alt="John Doe a son bureau" className="img-fluid w-100"/>  {/* j'ecrit l'alt comme si il s'agissait bel et bien d'une image de John Doe et non pas juste une stock image */}
+                        <article>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nisi beatae nemo cupiditate veniam laborum sequi? Eveniet omnis quas, cumque, dolor tempore vitae quae non, tempora perspiciatis officiis eius vel.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nisi beatae nemo cupiditate veniam laborum sequi? Eveniet omnis quas, cumque, dolor tempore vitae quae non, tempora perspiciatis officiis eius vel.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nisi beatae nemo cupiditate veniam laborum sequi? Eveniet omnis quas, cumque, dolor tempore vitae quae non, tempora perspiciatis officiis eius vel.</p>
+                        </article>
+                    </section>
+                    <section className="col-sm-6">
+                        <h3 className="fw-bold border-bottom border-primary border-5 pb-2 my-4">Mes compétences</h3>
+                        <article>
+                            <section className="my-4">
+                                <h4>HTML5 90%</h4>
+                                <div className="progress">
+                                <div className="progress-bar bg-danger" role="progressbar" style={{ width: '90%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </section>
+                            <section className="my-4">
+                                <h4>CSS3 80%</h4>
+                                <div className="progress">
+                                <div className="progress-bar bg-info" role="progressbar" style={{ width: '80%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </section>
+                            <section className="my-4">
+                                <h4>JAVASCRIPT 70%</h4>
+                                <div className="progress">
+                                <div className="progress-bar bg-warning" role="progressbar" style={{ width: '70%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </section>
+                            <section className="my-4">
+                                <h4>PHP 60%</h4>
+                                <div className="progress">
+                                <div className="progress-bar bg-success" role="progressbar" style={{ width: '60%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </section>
+                            <section className="my-4">
+                                <h4>REACT 50%</h4>
+                                <div className="progress">
+                                <div className="progress-bar" role="progressbar" style={{ width: '50%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </section>
+                        </article>
+                    </section>
+                </div>
+            </div>
         </div>
     )
 }
